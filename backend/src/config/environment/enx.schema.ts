22ba5ef5485faf387b6786d9d8f060adba6ebb63
vedantwadekar49@gmail.com <vedantwadekar49@gmail.com>
2026-08-03 23:41:28 +0530

    "Stage 7 – Authentication Module"

--- backend/src/config/environment/enx.schema.ts
+++ backend/src/config/environment/enx.schema.ts
@@ -8,6 +8,16 @@ export const envSchema = z.object({
   HOST: z.string().min(1),
 
   MONGODB_URI: z.string().min(1),
+
+  JWT_ACCESS_SECRET: z.string().min(10),
+
+  JWT_REFRESH_SECRET: z.string().min(10),
+
+  JWT_ACCESS_EXPIRES_IN: z.string(),
+
+  JWT_REFRESH_EXPIRES_IN: z.string(),
+
+  BCRYPT_SALT_ROUNDS: z.coerce.number(),
 });
 
 export type Env = z.infer<typeof envSchema>;
